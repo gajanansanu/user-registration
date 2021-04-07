aa0f900f62b4f997481690d4f47357ddad0dc1a7
gdsanu <gds.blr@gmail.com>
2021-04-07 14:57:16 +0530

    User Registration with full functionality committed

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import { Container } from "react-bootstrap";
+import SignUp from "./components/SignUp";
+import { AuthProvider } from "./contexts/AuthContext";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import SignIn from "./components/SignIn";
+import Dashboard from "./components/Dashboard";
+import PrivateRoute from "./components/PrivateRoute";
+import ForgotPassword from "./components/ForgotPassword";
+import UpdateProfile from "./components/UpdateProfile";
+
+
+function App() {
+  return (
+    
+      <Container className='d-flex align-items-center justify-content-center' style={{ minHeight: '100vh'}}>
+      <div className='w-100' style={{ maxWidth: '400px' }}>
+        <Router>
+          <AuthProvider>
+            <Switch>
+              <PrivateRoute exact path='/' component={Dashboard} />
+              <PrivateRoute path='/update-profile' component={UpdateProfile} /> 
+              <Route path='/signup' component={SignUp} />
+              <Route path='/signin' component={SignIn} />
+              <Route path='/forgot-password' component={ForgotPassword}/>
+              </Switch>
+          </AuthProvider>
+         </Router> 
+        </div>  
+      </Container> 
+  );
+}
+
+export default App;
---
+++ src/components/ForgotPassword.js
@@ -0,0 +1,55 @@
+
+import React, { useRef, useState } from 'react';
+import { Form, Button, Card, Alert } from 'react-bootstrap';
+import { useAuth } from '../contexts/AuthContext';
+import { Link, useHistory } from 'react-router-dom'
+
+
+const ForgotPassword = () => {
+    const emailRef = useRef();
+    const [error, setError] = useState('');
+    const [message, setMessage] = useState('');
+    const [loading, setLoading] = useState(false);
+    const { resetPassword } = useAuth();
+
+
+    async function handleSubmit(e) {
+        e.preventDefault()
+        try {
+            setMessage('');
+            setError('');
+            setLoading(true);
+            await resetPassword(emailRef.current.value);
+            setMessage('Check your inbox for futher instructions...');
+        } catch {
+            setError('Failed to reset password');
+        }
+        setLoading(false);
+     }
+    return (
+        <>
+            <Card>
+                <Card.Body>
+                    <h2 className='text-center mb-4'>Password Reset</h2>
+                    {error && <Alert variant='danger'>{error}</Alert>}
+                    {message && <Alert variant='success'>{message}</Alert>}
+                    <Form onSubmit={ handleSubmit}>
+                        <Form.Group id='email'>
+                            <Form.Label>Email ID</Form.Label>
+                            <Form.Control type='email' ref={emailRef} required></Form.Control>
+                        </Form.Group>
+                        
+                        <Button disabled={ loading }className='w-100' type='submit'>Reset</Button>
+                    </Form>
+                   
+                </Card.Body>                
+            </Card>
+            <div className='w-100 text-center mt-2'>
+                Remembered your password? <Link to='/signin'> Sign In</Link>
+            </div>
+                
+        </>
+    )
+}
+
+export default ForgotPassword;
